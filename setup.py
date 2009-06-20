970df1cb9aa58e4ce4c4017f0cc8051299b1d8aa
Brandon Bickford <bickfordb@gmail.com>
2009-06-20 11:45:48 -0700

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
+import os
+import sys
+
+setup(
+    name="waffle",
+    version='0.1',
+    url='http://github.com/bickfordb/waffle/',
+    author='Brandon Bickford',
+    author_email='bickfordb@gmail.com',
+    description='A library to store schema-less data in relational databases',
+    packages=['waffle'],
+    classifiers = [
+        'Development Status :: 1 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Programming Language :: Python',
+        'Topic :: Database', 
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]) 
